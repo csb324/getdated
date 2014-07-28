8727bf221790c15b8f7ff3f3f584a75129aa0300
csb324 <csb324@gmail.com>
2014-07-28 14:57:33 -0400

    Deleted redundant migration we don't need anymore

--- db/migrate/20140728154851_add_uid_to_user.rb
+++
@@ -1,9 +0,0 @@
-class AddUidToUser < ActiveRecord::Migration
-  def change
-    change_table :users do |t|
-      t.string :uid, index:true
-      t.string :name, index:true
-      t.string :provider
-    end
-  end
-end
