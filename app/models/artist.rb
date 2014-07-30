266aef3aaaabc78ee430c7adf7582e0879b2a847
csb324 <csb324@gmail.com>
2014-07-29 23:06:38 -0400

    Database no longer drops everything and rebuilds itself from scratch when user logs in
    * because that was silly
    * database also deletes tracks that are no longer there

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,8 +2,8 @@ class Artist < ActiveRecord::Base
   has_many :tracks
   has_and_belongs_to_many :genres
 
-  def self.refresh(current_user)
-    current_user.artists.uniq.each do |artist|
+  def self.refresh(new_artists)
+    new_artists.uniq.each do |artist|
       artist_json_string = RestClient.get "https://api.spotify.com/v1/artists/#{artist.spotify_id}"
       artist_hash = JSON.parse artist_json_string
       artist.genres = []
--- app/models/track.rb
+++ app/models/track.rb
@@ -4,8 +4,13 @@ class Track < ActiveRecord::Base
   belongs_to :artist
 
   def self.refresh(credentials, current_user)
-    current_user.tracks.destroy_all
+
+    returned_ids = []
+    new_artists = []
+    @user = User.find_by(uid: current_user.uid)
     @auth_token = "Bearer #{credentials.token}"
+
+
     playlists_json_string = RestClient::Request.execute(
       :method => :get,
       :url => "https://api.spotify.com/v1/users/#{current_user.uid}/playlists",
@@ -13,7 +18,9 @@ class Track < ActiveRecord::Base
     )
     playlists_hash = JSON.parse playlists_json_string
 
+
     # playlists_hash['items'] is an array of playlists
+
     playlists_hash['items'].each do |playlist|
       if playlist['owner']['id'] == current_user.uid
         tracks_json_string = RestClient::Request.execute(
@@ -23,28 +30,44 @@ class Track < ActiveRecord::Base
           )
         tracks_hash = JSON.parse tracks_json_string
 
-        @user = User.find_by(uid: current_user.uid)
         if tracks_hash['items'].empty? == false
           tracks_hash['items'].each do |track|
-            @newtrack = Track.new(spotify_id: track['track']['id'])
-            @newtrack.spotify_id = track['track']['id']
-            @newtrack.name = track['track']['name']
-            @newtrack.user = @user
 
-            # I would bet there's a better way to do this.
-            if track['track']['artists'].first['id'] != nil
-              @newtrack.artist = Artist.find_or_create_by(
+            returned_ids << track['track']['id']
+
+            @newtrack = Track.find_by(
+              spotify_id: track['track']['id'],
+              user: @user
+            )
+
+            if @newtrack == nil && track['track']['id'] != nil
+              @newtrack = Track.new(spotify_id: track['track']['id'])
+              @newtrack.spotify_id = track['track']['id']
+              @newtrack.name = track['track']['name']
+              @newtrack.user = @user
+
+              @newtrack.artist = Artist.find_by(
                 :spotify_id => track['track']['artists'].first['id'],
                 :name => track['track']['artists'].first['name'])
+              if @newtrack.artist == nil
+                @newtrack.artist = Artist.new(
+                  :spotify_id => track['track']['artists'].first['id'],
+                  :name => track['track']['artists'].first['name'])
+                new_artists << @newtrack.artist
+              end
+              @newtrack.save!
             end
-            @newtrack.save!
-
           end
         end
       end
     end
 
-    Artist.refresh(current_user)
+    to_delete = @user.tracks.map(&:spotify_id) - returned_ids
+    to_delete.each do |track|
+      Track.find_by(spotify_id: track).destroy
+    end
+
+    Artist.refresh(new_artists)
 
   end
 
