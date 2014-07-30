acc79305f948e5538c7f36edb99ffdf007fff217
EricRho <eric.rho.87@gmail.com>
2014-07-30 15:36:19 -0400

    Create favorites index getting all of our favorites

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,4 +2,13 @@ class Favorite < ActiveRecord::Base
   has_many :messages
   belongs_to :fav_initiator, :class_name => "User"
   belongs_to :fav_receiver, :class_name => "User"
+
+  def other_user(me)
+    if fav_initiator == me
+      fav_receiver
+    else
+      fav_initiator
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :members, :controller => 'users'
   match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
 
-  resources :favorites, only: [:index, :create, :destroy]
+  resources :favorites, only: [:index, :create, :destroy, :show]
 
   resources :messages, only: [:show, :index,:new, :create, :destroy]
 
