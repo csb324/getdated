057f608f6e4cbb271418428a9e8fcfb145d87d2f
csb324 <csb324@gmail.com>
2014-07-30 14:22:28 -0400

    Matcher refactoring. All nice and clean.

--- app/models/matcher.rb
+++ app/models/matcher.rb
@@ -4,10 +4,6 @@ class Matcher
     @user1, @user2 = user1, user2
   end
 
-  def tracks_count
-    shared_tracks.length
-  end
-
   def shared(option)
     if option == :artists
       @user1.artists & @user2.artists
@@ -39,7 +35,7 @@ class Matcher
   end
 
   def score
-    track_similarity = tracks_count / (((@user1.tracks.count + @user2.tracks.count) - tracks_count) * 1.0)
+    track_similarity = shared_count(:tracks) / (((@user1.tracks.uniq.count + @user2.tracks.uniq.count) - shared_count(:tracks)) * 1.0)
     artist_similarity = shared_count(:artists) / (((@user1.artists.uniq.count + @user2.artists.uniq.count) - shared_count(:artists)) * 1.0)
     genre_similarity = shared_count(:genres) / (((@user1.genres.uniq.count + @user2.genres.uniq.count) - shared_count(:genres)) * 1.0)
     (track_similarity * 4) + (artist_similarity * 2) + (genre_similarity * 1)
