5fc26acad5751c43bf929950fc1d2a7a50cac0fc
brimatteng <btenggren20@gmail.com>
2014-07-31 13:25:08 -0400

    Whooooops clara messed it up

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController <ApplicationController
   def index
-    potentials = current_user.potential_matches
-    @users = potentials.sort_by{ |user| user.match_with(current_user)}.reverse
+    if signed_in?
+      potentials = current_user.potential_matches
+      @users = potentials.sort_by{ |user| user.match_with(current_user)}.reverse
+    end
   end
 end
