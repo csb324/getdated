98eb4dfdabfefaa958ea825c77d1cb966eafa3b0
brimatteng <btenggren20@gmail.com>
2014-08-01 09:10:50 -0400

    Remove extraneous code and add coments

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
     if user_signed_in?
+      # gets potentials based on gender and interest and location
       potentials = current_user.potential_matches
       @users = potentials.sort_by{ |user| user.match_with(current_user)}.reverse
     end
